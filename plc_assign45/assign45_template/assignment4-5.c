ed1a5380d7533058bc5314ae80d7e935933ea045
mongooma <rhythm_qing@hotmail.com>
2019-04-02 16:30:01 -0400

    framework code finish, wait to debug

--- plc_assign45/assign45_template/assignment4-5.c
+++ plc_assign45/assign45_template/assignment4-5.c
@@ -21,7 +21,7 @@
 // #define BGQ 1 // when running BG/Q, comment out when testing on mastiff
 
 #ifdef BGQ
-#include<hwi/include/bqc/A2_inlines.h>
+#include <hwi/include/bqc/A2_inlines.h>
 #else
 #define GetTimeBase MPI_Wtime            
 #endif
@@ -42,12 +42,21 @@ double g_processor_frequency = 1600000000.0; // processing speed for BG/Q
 unsigned long long g_start_cycles=0;
 unsigned long long g_end_cycles=0;
 
+typedef struct update_args
+{
+    int ticks;
+    int thread_no;
+    int no_of_threads;
+    int rows;
+    int ** sub_universe; 
+}update_arg;
+
 // You define these
 
 int N = 1024;
 int thread_n  = 64;
 int ticks = 256;
-
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 /***************************************************************************/
 /* Function Decs ***********************************************************/
@@ -84,16 +93,17 @@ int main(int argc, char *argv[])
     
 
 // Allocate My rank’s chunk of the universe + space for "ghost" rows.
-    int ** sub_universe = calloc((int)(N / mpi_commsize) + 2, sizeof(int *));
-    for(int i = 0; i < (int)(N / mpi_commsize) + 2; i ++){
-        sub_universe[i] = calloc((int)(N / mpi_commsize), sizeof(int));
+    int rank_rows = (int)(N / mpi_commsize);
+    int ** sub_universe = calloc(rank_rows + 2, sizeof(int *));
+    for(int i = 0; i < rank_rows + 2; i ++){
+        sub_universe[i] = calloc(N, sizeof(int));
     }
     if(mpi_myrank == 0){
         int ** universe = calloc(N, sizeof(int *));
         for(int i = 0; i < N; i ++){
             universe[i] = calloc(N, sizeof(int));
 
-         GetTimeBase();
+        GetTimeBase();
     }
     
 
@@ -101,37 +111,55 @@ int main(int argc, char *argv[])
     //         void *recvbuf, int recvcount, MPI_Datatype recvtype, int root,
     //         MPI_Comm comm)
 
-        MPI_Scatter(universe, (int) (N / mpi_commsize), MPI_INT *, sub_universe+1,  // ghost rows
-                                (int) (N / mpi_commsize), MPI_INT *, 0, MPI_COMM_WORLD)
+        MPI_Scatter(universe, rank_rows, MPI_INT *, sub_universe+1,  // ghost rows
+                                rank_rows, MPI_INT *, 0, MPI_COMM_WORLD)
     }
 
 // Initalize the universe (including "ghost" rows) with every cell being ALIVE
-    for(int i = 0; i < (int)(N / mpi_commsize) + 2; i ++){
-        for(int j = 0; j < (int)(N / mpi_commsize); j ++){
+    for(int i = 0; i < rank_rows + 2; i ++){
+        for(int j = 0; j < N; j ++){
             sub_universe[i][j] = ALIVE;
         }
     }    
 
-// Create Pthreads here. All threads should go into the for-loop.
+// Create Pthreads here.
     pthread_t tid[thread_n];
+    int * ALIVE_cells = call(ticks, sizeof(int)); /* modified by threads */
+    int * ALIVE_cells_sum = call(ticks, sizeof(int));
+
 
     update_arg * args_ = calloc(thread_n, sizeof(update_arg));
     
+    /* typedef struct update_args{
+            int ticks;
+            int thread_no;
+            int no_of_threads;
+            int rows; // no. of rows per rank (include 2 ghost rows)
+            int ** sub_universe; 
+        }update_arg;
+    */
     for(int i = 0; i < thread_n; i ++){
-        args_[i].ticks = ;
-        /*todo*/
-
+        args_[i].ticks = ticks;
+        args_[i].thread_no = i;
+        args_[i].no_of_threads = thread_n;
+        args_[i].rows = rank_rows + 2;
+        args_[i].sub_universe = sub_universe; // thread share the rank's sub_universe and operate on it in parallel
+        args_[i].ALIVE_cells = ALIVE_cells; // thread modify time ticks ALIVE_cells using mutex lock
+        
         pthread_create(&tid[i], NULL, update, (void *) &args_[i]);
     }
 
     for(int i = 0; i < thread_n; i ++){
-        pthread_join(tid[i], ALIVE_cells);
+        pthread_join(tid[i], NULL);
     }
 
-    MPI_Reduce(ALIVE_cells_sujm, ALIVE_cells); /* todo*/
+    /* int MPI_Reduce(const void *sendbuf, void *recvbuf, int count,
+                      MPI_Datatype datatype, MPI_Op op, int root,
+                      MPI_Comm comm) */
+    MPI_Reduce(ALIVE_cells, ALIVE_cells_sum, ticks, MPI_INT, MPI_Sum, 0, MPI_COMM_WORLD);
 
 
-
+    /* some experiment required*/
 
 
     MPI_Barrier( MPI_COMM_WORLD );
@@ -156,7 +184,7 @@ void * update( void ** args_ ){
     args: 
     
     ticks
-    thread no -> local
+    thread no -> tid[i]:i
     no of threads per rank
     no of rows per *rank*
     sub_universe
@@ -171,6 +199,7 @@ void * update( void ** args_ ){
     int no_of_threads = args->no_of_threads;
     int rows = args->rows;
     int ** sub_universe = args->sub_universe;  
+    int * ALIVE_cells = args->ALIVE_cells;
 
     int mpi_myrank;
     int mpi_commsize;
@@ -209,13 +238,18 @@ void * update( void ** args_ ){
             
             /* int MPI_Isend(const void *buf, int count, MPI_Datatype datatype, int dest,
                 int tag, MPI_Comm comm, MPI_Request *request) */
-            /* int MPI_Irecv(void *buf, int count, MPI_Datatype datatype,
-               int source, int tag, MPI_Comm comm, MPI_Request *request) */
+            /* int MPI_Irecv(      void *buf, int count, MPI_Datatype datatype, int source, 
+                int tag, MPI_Comm comm, MPI_Request *request) */
             if(mpi_myrank == 0){
-                MPI_Isend(sub_universe[1], N, MPI_INT, mpi_commsize-1, MPI_tag, MPI_COMM_WORLD, &request);
+                MPI_Isend(sub_universe[1], N, MPI_INT, mpi_commsize - 1, MPI_tag, MPI_COMM_WORLD, &request);
             }else{
                 MPI_Isend(sub_universe[1], N, MPI_INT, mpi_myrank - 1, MPI_tag, MPI_COMM_WORLD, &request);   
             }
+            
+            /* int MPI_Wait(MPI_Request *request, MPI_Status *status)*/
+            MPI_Wait(&request, MPI_STATUS_IGNORE); // Wait block until request succeed, `request` deallocated
+            /* another way is to do while loop for MPI_Test and check `flag` */
+
             if(mpi_myrank == mpi_commsize-1){
                 MPI_Isend(sub_universe[rows-1], N, MPI_INT, 0, MPI_tag, MPI_COMM_WORLD, &request);
             }else{
@@ -223,14 +257,23 @@ void * update( void ** args_ ){
 
             }
 
-            while(1){
-                MPI_Irecv();
-                if() break; /*todo*/
+            MPI_Wait(&request, MPI_STATUS_IGNORE); // Wait block until request succeed, `request` deallocated
+
+            if(mpi_myrank == 0){
+                MPI_Irecv(sub_universe[0], N, MPI_INT, mpi_commsize - 1, MPI_tag, MPI_COMM_WORLD, &request);
+            }else{
+                MPI_Irecv(sub_universe[0], N, MPI_INT, mpi_myrank - 1, MPI_tag, MPI_COMM_WORLD, &request);
             }
-            while(1){
-                MPI_Irecv();
-                if() break; /*todo*/
+
+            MPI_Wait(&request, MPI_STATUS_IGNORE); // Wait block until request succeed, `request` deallocated
+
+            if(mpi_myrank == mpi_commsize-1){
+                MPI_Irecv(sub_universe[rows], N, MPI_INT, 0, MPI_tag, MPI_COMM_WORLD, &request);
+            }else{
+                MPI_Irecv(sub_universe[rows], N, MPI_INT, mpi_myrank + 1, MPI_tag, MPI_COMM_WORLD, &request);
             }
+
+            MPI_Wait(&request, MPI_STATUS_IGNORE); // Wait block until request succeed, `request` deallocated
 
         }
 
@@ -249,12 +292,130 @@ void * update( void ** args_ ){
         */
 
         /* update *simutanously* */
-        /* mind the corners ! -- asked on submitty */
-        int ** sub_universe_copy;
-        int thread_chunk = (int)(rows / thread_no);
+        int ** sub_universe_thread_part_copy; /* thread_chunk * N */
 
-        /*todo*/
-        /*****/
+        /* get global(in rank) thread row index 0*/
+        int thread_chunk = (int)((rows - 2) / thread_no); // exclude the ghost rows
+        int thread_rows_0 = thread_chunk * (thread_no - 1) + 1; // include the ghost row
+
+        /**RULES:
+
+        Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+        Any live cell with two or three live neighbors lives on to the next generation.
+        Any live cell with more than three live neighbors dies, as if by over-population.
+        Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+        **/
+
+        int living_nbrs;
+        int state;
+        int get_state(int previous_state, int living_nbrs){
+
+            if(living_nbrs < 2){
+                return DEAD;
+            }else if (living_nbrs == 2)
+            {
+                if(previous_state == ALIVE){
+                    return ALIVE;
+                }else{
+                    return DEAD;
+                }
+            }else if(living_nbrs == 3){
+                return ALIVE;
+            }else if(living_nbrs > 3){
+                return DEAD;
+            }
+
+        }
+
+        int alives;
+
+        for(int i = 0; i < thread_chunk; i ++ ){ /* thread local row index*/
+            /*
+            +++++++++++++++++++++++++++++++ row = 0 (rank ghost row)
+            ...............................
+                      ...
+            ...............................
+            ...............................  i = 1
+                      ...
+            ...............................  i = thread_chunk
+            ...............................
+                      ...
+            ...............................  
+            +++++++++++++++++++++++++++++++ row = rows (rank ghost row)
+
+
+            123
+            8_4
+            765  ---> eight nbrs
+
+            */
+            for(int j = 0: j < N; j ++){
+                living_nbrs = 0
+
+                /* 2: previous row corresponding cell */
+                living_nbrs += sub_universe[thread_rows_0 + i - 1][j];
+                /* 6: next row corresponding cell */
+                living_nbrs += sub_universe[thread_rows_0 + i + 1][j];
+
+                if((j == 0) || (j == N-1)){ /* row ends; utilizing the end of the rows for missing left/right nbrs */
+                    if(j == 0){
+                        /* 1: previous row last cell*/ 
+                        living_nbrs += sub_universe[thread_rows_0 + i - 1][N-1]; //
+                        /* 3: previous next cell */
+                        living_nbrs += sub_universe[thread_rows_0 + i - 1][j+1];
+                        /* 4: current row next cell */
+                        living_nbrs += sub_universe[thread_rows_0 + i ][j+1];
+                        /* 5: next row next cell */
+                        living_nbrs += sub_universe[thread_rows_0 + i + 1][j+1];
+                        /* 7: next row last cell */ 
+                        living_nbrs += sub_universe[thread_rows_0 + i + 1][N-1]; //
+                        /* 8: current row last cell */ 
+                        living_nbrs += sub_universe[thread_rows_0 + i][N+1]; //
+                    }
+                    else{
+                        /* 1: previous row previous cell*/
+                        living_nbrs += sub_universe[thread_rows_0 + i - 1][j-1];
+                        /* 3: previous head cell */
+                        living_nbrs += sub_universe[thread_rows_0 + i - 1][0]; //
+                        /* 4: current row head cell */
+                        living_nbrs += sub_universe[thread_rows_0 + i ][0]; //
+                        /* 5: next row head cell */
+                        living_nbrs += sub_universe[thread_rows_0 + i + 1][0]; //
+                        /* 7: next row previous cell */
+                        living_nbrs += sub_universe[thread_rows_0 + i + 1][j-1];
+                        /* 8: current row previous cell */
+                        living_nbrs += sub_universe[thread_rows_0 + i][j-1];
+
+                    }
+
+                }else{
+                    /* 1: previous row previous cell*/
+                    living_nbrs += sub_universe[thread_rows_0 + i - 1][j-1];
+                    /* 3: previous next cell */
+                    living_nbrs += sub_universe[thread_rows_0 + i - 1][j+1];
+                    /* 4: current row next cell */
+                    living_nbrs += sub_universe[thread_rows_0 + i ][j+1];
+                    /* 5: next row next cell */
+                    living_nbrs += sub_universe[thread_rows_0 + i + 1][j+1];
+                    /* 7: next row previous cell */
+                    living_nbrs += sub_universe[thread_rows_0 + i + 1][j-1];
+                    /* 8: current row previous cell */
+                    living_nbrs += sub_universe[thread_rows_0 + i][j-1];
+                }
+
+                state = get_state(sub_universe[thread_rows[0] + i + 1][j], living_nbrs);
+                /* thread_rows index from non-ghost row*/
+                sub_universe_thread_part_copy[thread_rows[0] + i + 1][j] = state;
+
+                if(state == ALIVE){
+                    alives += 1;
+                }
+
+
+            }
+
+        }
 
 
         /*
@@ -264,15 +425,15 @@ void * update( void ** args_ ){
 
         */ 
 
-        ALIVE_cells[i] += alives;
-
-        /*todo*/
+        pthread_mutex_lock( &lock);
+        ALIVE_cells[i] += alives; /*i : index of ticks*/
+        pthread_mutex_unlock( &lock);
 
 
     } /*tick end*/
 
 
-    return &ALIVE_cells; // each rank; int[ticks] -> no. of alive cells at each time tick
+    return NULL;
 
 }
 
